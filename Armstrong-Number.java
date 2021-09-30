c5b69d57f3afbb77447be319bc1931d25220de8e
vaibhavss08 <81666621+vaibhavss08@users.noreply.github.com>
2021-10-01 00:47:04 +0530

    Update Armstrong-Number.java

--- Armstrong-Number.java
+++ Armstrong-Number.java
@@ -4,24 +4,19 @@ public class Armstsrong
 {  
 	static boolean isArmstrong(int n)   
 	{   
-		int temp, digits=0, last=0, sum=0;    
-		temp=n;   
-		while(temp>0)    
-		{   
-			temp = temp/10;   
-			digits++;   
-		}   
-		temp = n;   
-		while(temp>0)   
-		{       
-			last = temp % 10;   
-			sum +=  (Math.pow(last, digits));   
-			temp = temp/10;   
-		}  
-		if(n==sum)   
-			return true;       
-		else 
-			return false;   
+		int m=n;
+		int sum=0;
+		while(m>0){
+			int x=m%10;
+			sum+=Math.pow(x,3);
+			m/=10;
+		}
+	
+		if(sum==n){
+			return true;
+		}else{
+			return false;
+		}
 	}    
 	public static void main(String args[])     
 	{     
